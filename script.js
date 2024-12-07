4e128eaa164d243ec57009213244e24657c39d87
AKSHAT249 <92660619+AKSHAT249@users.noreply.github.com>
2024-12-07 17:05:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function handlePromise(text, delay){
+	return new Promise((resolve, reject)=>{
+		setTimeout(()=>{
+			resolve(text);
+		},delay)
+	})
+}
+
+document.getElementById("btn").addEventListener("click",async ()=> {
+	const text = document.getElementById("text").value;
+	const delay = document.getElementById("delay").value;
+
+	let output = await handlePromise(text, delay);
+	document.getElementById("output").innerHTML = output;
+
+
+
+})
